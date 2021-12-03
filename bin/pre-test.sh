f07d68d7b16a5418ada986dde89b4950d1b4c5e8
Thiago Monteiro de Paula <thiago.paula@luizalabs.com>
2021-12-03 18:38:43 -0300

    chore: removes .env.test

--- bin/pre-test.sh
+++ bin/pre-test.sh
@@ -1,9 +1,9 @@
-echo "## Setting test envs"
-. ./.env.test
+if [ -f ".env.test" ]; then
+  . ./.env.test
+fi
 
 echo "## Executing database migrations"
 npm run runMigrations
 
 echo "## Running pre-test scripts"
 ts-node jest/jest.pretest.ts
-
