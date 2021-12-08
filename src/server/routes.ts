76fe8038ac2fc01334e69109539f83e5c8732862
Thiago Monteiro de Paula <thiago.paula@luizalabs.com>
2021-12-08 19:36:01 -0300

    docs: creates swagger API docs

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 
 import userRouter from '../user/user.routes';
 import messageRouter from '../message/message.routes';
 import healthcheckRouter from '../healthcheck/healthcheck.routes';
 
 const router = express.Router();
+const swaggerDocument = yaml.load('src/docs/swagger.yml');
 
 router.use('/user', userRouter);
 router.use('/message', messageRouter);
 router.use('/', healthcheckRouter);
+
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
 
 export default router;
